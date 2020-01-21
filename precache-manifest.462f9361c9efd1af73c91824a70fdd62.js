7073b8a8b8713bbc151f65f5c8cf877cbcadb406
Andrii Lundiak <landike@gmail.com>
2020-01-21 19:48:08 +0100

    Updates

---
+++ precache-manifest.462f9361c9efd1af73c91824a70fdd62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521de302c4f848d92b9a0a77ab7e7c0a",
+    "url": "/alundiak/drinks/index.html"
+  },
+  {
+    "revision": "11bb93ace0d3f20bf25d",
+    "url": "/alundiak/drinks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "419445bd1687552246c6",
+    "url": "/alundiak/drinks/static/js/2.cbc44666.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/alundiak/drinks/static/js/2.cbc44666.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11bb93ace0d3f20bf25d",
+    "url": "/alundiak/drinks/static/js/main.313bc7fa.chunk.js"
+  },
+  {
+    "revision": "83799bc075737beb3591",
+    "url": "/alundiak/drinks/static/js/runtime-main.84695a54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alundiak/drinks/static/media/logo.5d5d9eef.svg"
+  }
+]);
