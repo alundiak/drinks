208e3e877ca84128376b309edb85f134a9d341a2
Andrii Lundiak <landike@gmail.com>
2023-03-25 22:12:47 +0100

    Small cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Drinks component', () => {
   const { container } = render(<App />);
   const header = container.querySelector('header');
   expect(header).toBeInTheDocument();
