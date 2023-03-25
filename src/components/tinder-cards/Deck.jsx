c5a97e1e57c32748eb5ab9fcb516199684800ed9
Andrii Lundiak <landike@gmail.com>
2023-03-25 22:33:06 +0100

    NEW Tinder/Deck Component VERSION based on PROPER code of  and  usage

--- src/components/tinder-cards/Deck.jsx
+++ src/components/tinder-cards/Deck.jsx
@@ -7,20 +7,26 @@ import { useSprings } from "@react-spring/web";
 import { useDrag } from "@use-gesture/react";
 // https://use-gesture.netlify.app/docs/
 
+// Full example (already based on react v18 but different versions of other packages):
+// https://codesandbox.io/s/github/pmndrs/use-gesture/tree/main/demo/src/sandboxes/cards-stack
+
 import Card from "./Card";
 import data from "./data.js";
 
 import "./Deck.css";
 
+// These two are just helpers, they curate spring data, values that are later being interpolated into css
 const to = i => ({
   x: 0,
-  y: i * -10,
+  y: i * -10, // -4 in example
   scale: 1,
   rot: -10 + Math.random() * 20,
   delay: i * 100
 });
 const from = i => ({ rot: 0, scale: 1.5, y: -1000 });
+// const from = _i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 }); // in example x: 0 ADDED
 
+// This is being used down there in the view, it interpolates rotation and scale into a css transform
 const trans = (r, s) =>
   `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`;
 
@@ -36,50 +42,39 @@ function Deck() {
     []
   );
 
-  // According new API from @use-gesture
-  // const bind = useDrag(
-  //   ({ down, movement: [x], args: [index] }) => api.start((i) => i === index && { x: down ? x : 0 }),
-  //   { axis: 'x' }
-  // )
+  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
+  const bind = useDrag(({ args: [index], active, movement: [mx], direction: [xDir], velocity: [vx] }) => {
+    const trigger = vx > 0.2 // If you flick hard enough it should trigger the card to fly out
 
-  const bind = useDrag(
-    ({
-      args: [index],
-      down,
-      delta: [xDelta],
-      distance,
-      direction: [xDir],
-      velocity
-    }) => {
-      const trigger = velocity > 0.2;
+    if (!active && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
 
-      const dir = xDir < 0 ? -1 : 1;
+    api.start(i => {
+      if (index !== i) {
+        return; // We're only interested in changing spring-data for the current spring
+      }
 
-      if (!down && trigger) gone.add(index);
+      const isGone = gone.has(index);
+      const x = isGone ? (200 + window.innerWidth) * xDir : active ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+      const rot = mx / 100 + (isGone ? xDir * 10 * vx : 0); // How much the card tilts, flicking it harder makes it rotate faster
+      const scale = active ? 1.1 : 1; // Active cards lift up a bit
 
-      api.start(i => {
-        if (index !== i) return;
-        const isGone = gone.has(index);
+      return {
+        x,
+        rot,
+        scale,
+        delay: undefined,
+        config: { friction: 50, tension: active ? 800 : isGone ? 200 : 500 },
+      };
+    });
 
-        const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0;
+    if (!active && gone.size === data.length) {
+      setTimeout(() => {
+        gone.clear()
+        api.start(i => to(i))
+      }, 600);
+    }
 
-        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0);
-
-        const scale = down ? 1.1 : 1;
-        return {
-          x,
-          rot,
-          scale,
-          delay: undefined,
-          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 }
-        };
-      });
-
-      if (!down && gone.size === data.length)
-        setTimeout(() => gone.clear() || api.start(i => to(i)), 600);
-    },
-    { axis: 'x' }
-  );
+  });
 
   return springs.map(({ x, y, rot, scale }, i) => (
     <Card
